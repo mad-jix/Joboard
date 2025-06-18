692e559661028b6c43959abbb14783c73f952240
mad-jix <jackjaseelk@gmail.com>
2025-06-18 17:53:29 +0530

    static

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -128,7 +128,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # if you have additional static dirs
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
